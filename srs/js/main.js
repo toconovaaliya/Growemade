98b06053fc3a49d0a2e8e7526eecf01ed3c35cb6
Aliya Tokonova <aliyatoconova346@gmail.com>
2022-03-20 00:36:13 +0600

    first

---
+++ srs/js/main.js
@@ -0,0 +1,12 @@
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 25,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+
